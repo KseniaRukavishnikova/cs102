88b35c2f904a35337f9dd2ffc2620c989d825761
KseniaRukavishnikova <ksynia41@gmail.com>
2018-10-26 23:13:10 +0300

    Реализована функция is_prime(n)

---
+++ homework01/rsa.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    """
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+    pass
+
